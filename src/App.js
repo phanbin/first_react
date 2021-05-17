13296839bcab99058fd18a51f267e1b8bc41a61e
cnvs_binh <phanvanbinh.canvas@gmail.com>
2021-05-17 12:04:49 +0700

    add files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
 
+import TodoItem from './components/todoItem';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const todoItems = [
+    {title: 'item 1', isComplete: true},
+    {title: 'item 2', isComplete: false},
+    {title: 'item 3', isComplete: false}
+  ];
+  // if(todoItems.length > 0){
+  //   return (
+  //     <div className="App">
+  //       <header className="App-header">
+  //         <p>To do list</p>
+  //         {
+  //           todoItems.map((item, index) => <TodoItem key={index} val={item}></TodoItem>)
+  //         }
+  //       </header>
+  //     </div>
+  //   );
+  // }else{
+  //   return (
+  //     <div className="App">
+  //       nothing here
+  //     </div>
+  //   );
+  // }
+  return <div className="App">
+    <header className="App-header">
+    {todoItems.length >0 && todoItems.map((item, index) =>
+      <TodoItem key={index} val={item}></TodoItem>
+    )}
+    {todoItems.length == 0 && 'nothing here'}
+    </header>
+  </div>
 }
 
 export default App;
---
+++ src/components/todoItem.js
@@ -0,0 +1,16 @@
+import './todoItem.css';
+
+function TodoItem(props) {
+    console.log(props);
+    let className = 'TodoItem';
+    if(props.val.isComplete){
+        className += ' complete-item';
+    }
+  return (
+    <div className= {className}>
+      <p>{props.val.title}</p>
+    </div>
+  );
+}
+
+export default TodoItem;
